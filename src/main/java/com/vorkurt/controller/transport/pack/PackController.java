068171314a9a879647dc34c30fc2cd11c3fb4f1c
SamiHSO <nechiforelsamuel@yahoo.com>
2022-05-04 00:19:09 +0300

    feat(pack-service): added controller and service for packs

--- src/main/java/com/vorkurt/controller/transport/pack/PackController.java
+++ src/main/java/com/vorkurt/controller/transport/pack/PackController.java
@@ -1,13 +1,18 @@
 package com.vorkurt.controller.transport.pack;
 
+import com.vorkurt.entity.transport.pack.Pack;
+import com.vorkurt.entity.transport.pack.response.PackResponse;
 import com.vorkurt.service.pack.PackService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/transport/pack")
@@ -17,4 +22,10 @@ public class PackController {
 
     @Autowired
     PackService packService;
+
+    @GetMapping()
+    public List<PackResponse> getAllPacks(){
+        logger.debug("Method get all packs");
+        return this.packService.getAllPacks();
+    }
 }
--- src/main/java/com/vorkurt/service/pack/PackService.java
+++ src/main/java/com/vorkurt/service/pack/PackService.java
@@ -1,11 +1,41 @@
 package com.vorkurt.service.pack;
 
+import com.vorkurt.entity.transport.pack.Pack;
+import com.vorkurt.entity.transport.pack.response.PackResponse;
 import com.vorkurt.repository.transport.pack.PackRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class PackService {
+
+    Logger logger = LoggerFactory.getLogger(PackService.class);
+
     @Autowired
     PackRepository packRepository;
 
+    /**
+     * Retrieve all packs
+     * @return List of type Pack.
+     */
+    public List<PackResponse> getAllPacks() {
+        logger.debug("Get All Pack");
+        List<Pack> daoPack = packRepository.findAll();
+        List<PackResponse> packResponses = new ArrayList<>();
 
+        daoPack.stream().forEach(pack -> {
+            if (!pack.isRepayment()){
+                pack.setRepayment(false);
+            }
+            packResponses.add(new PackResponse(pack));
+        });
+
+        return  packResponses;
+
+    }
 }
