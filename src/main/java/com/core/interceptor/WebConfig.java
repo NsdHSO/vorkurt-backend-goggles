2e5f2b0ef1049be87c29ce2ae96fd9ebaf83f65a
( Nechiforel David-Samuel ) NsdHSO <37635083+NsdHSO@users.noreply.github.com>
2022-05-07 02:03:44 +0300

    feat(interceptor): added component (#6)
    * feat(interceptor): added component

---
+++ src/main/java/com/core/interceptor/WebConfig.java
@@ -0,0 +1,36 @@
+package com.core.interceptor;
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Component
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    RetrieveDataFromUserInterceptor retrieveDataFromUserInterceptor;
+
+    Logger LOGGER = LoggerFactory.getLogger(WebConfig.class);
+
+    WebConfig(){
+        LOGGER.debug(WebConfig.class + " Init");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(...)
+//    ...
+//        registry.addInterceptor(getYourInterceptor());
+        registry.addInterceptor(retrieveDataFromUserInterceptor);
+        // next two should be avoid -- tightly coupled and not very testable
+        registry.addInterceptor(new RetrieveDataFromUserInterceptor());
+        registry.addInterceptor(new HandlerInterceptor() {
+        });
+    }
+}
