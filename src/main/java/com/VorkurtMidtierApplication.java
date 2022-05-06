43abce713d4479a06d9f79321bdaf4ccd6a6fbb4
SamiHSO <nechiforelsamuel@yahoo.com>
2022-05-07 01:41:25 +0300

    fix: added maven surefire

--- src/main/java/com/VorkurtMidtierApplication.java
+++ src/main/java/com/VorkurtMidtierApplication.java
@@ -1,16 +1,18 @@
 package com;
 
 import com.vorkurt.service.send.email.EmailServiceImplementation;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication()
 @ComponentScan({"com.vorkurt.controller", "com.vorkurt.service"})
 @EntityScan({"com.vorkurt.entity",})
 @EnableJpaRepositories({"com.vorkurt.repository"})
