11483790b53e2a2ffd8c2762b523a2440fe7ef4f
NsdHSD <nechiforelsamuel@yahoo.com>
2022-05-23 01:45:15 +0300

    feat: added rest controller

--- src/main/java/com/vorkurt/controller/auth/AuthController.java
+++ src/main/java/com/vorkurt/controller/auth/AuthController.java
@@ -7,8 +7,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+@RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
