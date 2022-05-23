5bb03d8fcb4cb97b01ee8f8de41e3929b5eda655
( Nechiforel David-Samuel ) NsdHSO <37635083+NsdHSO@users.noreply.github.com>
2022-05-23 03:28:53 +0300

    fix: secret key [VRBDEV-02] (#12)

--- src/main/java/com/vorkurt/controller/auth/AuthController.java
+++ src/main/java/com/vorkurt/controller/auth/AuthController.java
@@ -3,6 +3,7 @@ package com.vorkurt.controller.auth;
 import com.vorkurt.entity.auth.Auth;
 import com.vorkurt.service.send.email.EmailServiceImplementation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,12 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthController {
 
+    @Value("${data.email.password}")
+    private String password;
+
+    @Value("${spring.mail.username}")
+    private String sendFrom;
     @PostMapping
     public Auth sendEmail(@RequestBody Auth auth){
         new EmailServiceImplementation.EmailServiceImplementationBuilder()
                 .body(auth.getMessage())
                 .title(auth.getTitle())
                 .setTo(auth.getEmail())
+                .setFrom(sendFrom)
+                .setPassword(password)
                 .build() ;
         return new Auth();
     }
